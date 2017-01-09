fa15c17b4154ae488bb7e5a8051e10a491796fae
neonrights <nicholasfarn@gmail.com>
2017-01-08 17:58:43 -0800

    ENH edited data creation

--- create_data.py
+++ create_data.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import numpy as np
+from scipy import sparse
 
 input_path = 'test_images/'
 output_path = 'test_unlabeled/'
@@ -8,27 +9,36 @@ output_path = 'test_unlabeled/'
 file_names = os.listdir(input_path)
 for file_name in file_names:
 	# load in image and convert to binary
-	img = cv2.imread(file_name)
-	gray_scale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	binary = cv2.threshold(gray_scale, cv2.THRESH_BINARY)
+	print(file_name)
+	img = cv2.imread(input_path + file_name, cv2.IMREAD_GRAYSCALE)
+	_, binary = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 
 	# display image to make sure its good
-	cv2.imshow("binary conversion", binary)
+	cv2.imshow("graph", img)
 	# if image is good continue processing, otherwise reprocess with different method
 	# select locations of centered nodes
 
-	cv2.waitkey(0)
-	cv2.destroyAllWidows()
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
 
-	# label all other locations as 0, no node
-	# label locations as 1, centered node
+	output_name = output_path + file_name[:-4] + '-50x50.dat'
+	os.makedirs(os.path.dirname(output_name), exist_ok=True)
+	#with open(output_name, 'w+') as output_file:
+	to_save = list()
 	filter_size = 50
 	count = 0
 	width, height = img.shape
 	for i in range(width-filter_size):
 		for j in range(height-filter_size):
 			count += 1
-			cv2.imwrite(output_path+'sample-'+count+'.png', binary[i:(i+filter_size),j:(j+filter_size)])
+			stencil = binary[i:(i+filter_size),j:(j+filter_size)].flatten()
+			stencil[stencil == 0] = 1
+			stencil[stencil == 255] = 0
+			sparse = stencil.nonzero()[0]
+			if sparse.size != 0:
+				to_save.append(sparse)
+	
+	np.savez(output_name, tuple(to_save))
 
 # finished message
 print("completed processing all images")
